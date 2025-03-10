a6169399b4098af5742ca17e3fcdd138a09526ea
Gokul8807 <gokulvijayakumar76@gmail.com>
2025-03-10 22:05:36 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,8 @@
+function land(lenth,width)
+{
+    var a=lenth*widt
+    console.log("area is:"+a);
+}
+var length=20
+var widt=30
+land(20,30)
---
+++ oddoreven.js
@@ -0,0 +1,6 @@
+let num = prompt("Enter a number:");
+if (num % 2 === 0) {
+    alert(num + " is even.");
+} else {
+    alert(num + " is odd.");
+}
